19db45d9fe156d97ecec51952cd401fd14ec65ab
Nader Zare <nader.zare88@gmail.com>
2020-10-04 08:49:56 -0300

    change in merger, using pandas

--- script/data_extractor_merger.py
+++ script/data_extractor_merger.py
@@ -1,29 +1,21 @@
 import os
+import pandas as pd
 
-
-read_path =      '/home/aref/Documents/data/'
-save_file_path = '/home/aref/Documents/data/merged_data.csv'
+read_path =      '/media/nader/8cf88936-ff21-4a0a-a07a-1dbec4247478/robo_data/data/'
+save_file_path = '/media/nader/8cf88936-ff21-4a0a-a07a-1dbec4247478/robo_data/merged_data.csv'
 
 header = ''
 
 read_rows = []
 file_counter = 0
 files = os.listdir(read_path)
-for file in files:
-    file_counter += 1
-    print(file_counter / len(files) * 100)
-    open_file = open(read_path + file, 'r')
-    lines = open_file.readlines()
-    if header == '':
-        header = lines[0].rstrip('\n')
-    for line in lines[1:]:
-        read_rows.append(line.rstrip('\n'))
-    open_file.close()
 
-if not os.path.exists(save_file_path):
-    header = [header]
-    read_rows = header + read_rows
-
-out_file = open(save_file_path, 'a')
-out_file.write('\n'.join(read_rows))
-out_file.close()
+os.chdir(read_path)
+dfs = [pd.read_csv(f) for f in os.listdir(os.getcwd()) if f.endswith('csv')]
+for df in dfs:
+    del df[df.columns[-1]]
+print('end read')
+df = pd.concat(dfs, axis=0, join='inner')
+df = df.reset_index()
+del df['index']
+df.to_csv(save_file_path)
